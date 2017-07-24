22b52e5422d3081e9615da3c906a5a08eb06da9f
Joe Wass <jwass@crossref.org>
2017-07-24 16:26:07 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Event Data Evidence Log Snapshot
+
+Snapshots the Evidence Log into archive files.
+
+Runs daily. Ingests the entire Evidence Log Kafka topic and saves each day's worth into a snapshot in S3.
+
+## License
+
+Copyright © 2017 Crossref
+
+Distributed under the The MIT License (MIT).
---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject event-data-evidence-log-snapshot "0.1.0-SNAPSHOT"
+  :description "Event Data Evidence Log Snapshot"
+  :url "http://eventdata.crossref.org/"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [event-data-common "0.1.30"]
+                 [overtone/at-at "1.2.0"]
+                 [robert/bruce "0.8.0"]
+                 [yogthos/config "0.8"]
+                 [clj-time "0.12.2"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.apache.logging.log4j/log4j-core "2.6.2"]
+                 [org.slf4j/slf4j-simple "1.7.21"]
+                 [org.apache.kafka/kafka-clients "0.10.2.0"]]
+
+  :main ^:skip-aot event-data-evidence-log-snapshot.core
+  :target-path "target/%s")
