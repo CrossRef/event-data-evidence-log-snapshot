26ddaa1dc7fff9f7e189eb33ad02d856623431eb
Joe Wass <jwass@crossref.org>
2017-07-28 10:43:25 +0100

    Add Dockerfile, version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-evidence-log-snapshot "0.1.0-SNAPSHOT"
+(defproject event-data-evidence-log-snapshot "0.1.0"
   :description "Event Data Evidence Log Snapshot"
   :url "http://eventdata.crossref.org/"
   :license {:name "MIT License"
